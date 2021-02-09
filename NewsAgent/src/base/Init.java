4b4dbc6cf1c6ad6969d4ceb8fc8506f6c9085a7c
Sachin Kumar <A00279809@student.ait.ie>
2021-02-09 19:14:35 +0000

    Fix the previous error

---
+++ NewsAgent/src/base/Init.java
@@ -0,0 +1,13 @@
+package base;
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO this will be main class
+		// Call login from here
+		System.out.println(MysqlJDBC.getConnection());
+		MysqlJDBC.closeConnection();
+		System.out.println(MysqlJDBC.getConnection());
+	}
+
+}
