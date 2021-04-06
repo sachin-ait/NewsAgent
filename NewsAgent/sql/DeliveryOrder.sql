2189a962e3f07bebd9c2069ef144acfa3eeaa1ab
Niall-ait <72978449+Niall-ait@users.noreply.github.com>
2021-04-06 15:14:07 +0100

    Add files via upload
    Delivery Order SQL table

--- NewsAgent/sql/DeliveryOrder.sql
+++ NewsAgent/sql/DeliveryOrder.sql
@@ -1,10 +1,14 @@
+USE newsagent;
 DROP TABLE if exists DeliveryOrder;
 CREATE TABLE DeliveryOrder(
     DeliveryOrderId int NOT NULL AUTO_INCREMENT,
+    CustID int(4),
     CustName varchar(30),
+    CustAddress varchar(30),
     PublicationName varchar(30),
-    DeliveryDate varchar(12),     
+    DeliveryDate varchar(18),     
     PRIMARY KEY (DeliveryOrderId)
 );
-
-INSERT INTO DeliveryOrder(DeliveryOrderId,CustName,PublicationName,DeliveryDate) VALUES (1,'Jack','Starry', '13/2/2021');
+/*
+INSERT INTO DeliveryOrder(DeliveryOrderId,CustID,CustName,PublicationName,DeliveryDate) VALUES (1,'Jack', 'Athlone','Starry', '13/2/2021');
+*/
